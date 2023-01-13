27ab3fcc047196ea4ae2d436dd351469ab3624dd
Bruce Lam <xiaohuilam@users.noreply.github.com>
2023-01-14 02:02:10 +0800

    Update laravel.php

--- src/frameworks/laravel.php
+++ src/frameworks/laravel.php
@@ -3,16 +3,22 @@
 require_once __DIR__ . '/../../../../../vendor/autoload.php';
 
 use Adapterman\Adapterman;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use Tymon\JWTAuth\JWT;
 use Workerman\Worker;
 
 Adapterman::init();
 
-$http_worker                = new Worker('http://0.0.0.0:8080');
+$port = trim(shell_exec("cat " . __DIR__ . '/../../../../../' . ".env | grep HTTP_PORT | cut -d '=' -f2") ?: '8080');
+$host = trim(shell_exec("cat " . __DIR__ . '/../../../../../' . ".env | grep HTTP_HOST | cut -d '=' -f2") ?: '0.0.0.0');
+
+$http_worker                = new Worker('http://' . $host . ':' . $port);
 $http_worker->count         = 8;
 $http_worker->name          = 'AdapterMan';
 
 $http_worker->onMessage = static function ($connection, $request) {
-    $connection->send(run());
+    $connection->send(run($request));
 };
 
 $app = require_once __DIR__.'/../../../../../bootstrap/app.php';
@@ -21,20 +27,120 @@ global $kernel;
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-
-function run()
+function staticFile($path)
 {
+    $parse = parse_url($path);
+    $result = [];
+    if ($parse && isset($parse['path'])) {
+        if (is_file(file_get_contents(__DIR__ . '/../../../../../public' . $parse['path'])) && Str::endsWith(Str::lower($parse['path']), [
+            '.css', '.js',
+            '.png', '.jpg', '.jpeg', '.gif', '.svg', '.ico',
+            '.woff', '.woff2', '.ttf', '.eot', '.otf',
+            '.map',
+            '.txt', '.xml', '.json',
+            '.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx',
+            '.zip', '.rar', '.7z', '.gz', '.tar', '.bz2',
+        ]) ) {
+            $result = [
+                'content' => file_get_contents(__DIR__ . '/../../../../../public' . $parse['path'])
+            ];
+
+            $ext = Arr::last(explode('.', $parse['path']));
+
+            $mimeTypes = [
+                'css' => 'text/css',
+                'js' => 'application/javascript',
+                'png' => 'image/png',
+                'jpg' => 'image/jpeg',
+                'jpeg' => 'image/jpeg',
+                'gif' => 'image/gif',
+                'svg' => 'image/svg+xml',
+                'ico' => 'image/x-icon',
+                'woff' => 'application/font-woff',
+                'woff2' => 'application/font-woff2',
+                'ttf' => 'application/x-font-ttf',
+                'eot' => 'application/vnd.ms-fontobject',
+                'json' => 'application/json',
+                'map' => 'application/json',
+                'txt' => 'text/plain',
+                'xml' => 'application/xml',
+                'pdf' => 'application/pdf',
+                'doc' => 'application/msword',
+                'docx' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                'xls' => 'application/vnd.ms-excel',
+                'xlsx' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                'ppt' => 'application/vnd.ms-powerpoint',
+                'pptx' => 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+                'zip' => 'application/zip',
+                'rar' => 'application/x-rar-compressed',
+                '7z' => 'application/x-7z-compressed',
+                'gz' => 'application/gzip',
+                'tar' => 'application/x-tar',
+                'bz2' => 'application/x-bzip2',
+            ];
+            $result['content-type'] = $mimeTypes[$ext] ?? 'text/plain';
+
+            return $result;
+        }
+    }
+
+    return false;
+}
+
+function run($workermanRequest)
+{
+    Illuminate\Http\Request::setFactory(function () use ($workermanRequest) {
+        return new Illuminate\Http\Request(
+            $workermanRequest['get'],
+            $workermanRequest['post'],
+            $workermanRequest['request'],
+            $workermanRequest['cookie'],
+            $workermanRequest['files'],
+            $workermanRequest['server'],
+            (isset($workermanRequest['server']['CONTENT_TYPE']) && Str::contains($workermanRequest['server']['CONTENT_TYPE'], 'application/json')) ? json_encode($workermanRequest['post']) : null
+        );
+    });
+
+    $request = Illuminate\Http\Request::capture();
+
+    if ($request->method() === 'GET') {
+        $parse = parse_url($request->getRequestUri());
+        if ($parse && isset($parse['path'])) {
+            if (Str::endsWith(Str::lower($parse['path']), [
+                '.css', '.js',
+                '.png', '.jpg', '.jpeg', '.gif', '.svg', '.ico',
+                '.woff', '.woff2', '.ttf', '.eot', '.otf',
+                '.map',
+                '.txt', '.xml', '.json',
+                '.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx',
+                '.zip', '.rar', '.7z', '.gz', '.tar', '.bz2',
+            ]) ) {
+                $result = staticFile($parse['path']);
+                if ($result) {
+                    if (isset($result['content-type'])) {
+                        header('Content-Type: ' . $result['content-type']);
+                    }
+                    if (isset($result['content'])) {
+                        return $result['content'];
+                    }
+                }
+            }
+        }
+    }
+
+    if (class_exists(JWT::class)) {
+        JWT::customTokenParser(fn () => $request->bearerToken());
+    }
+
     global $kernel;
 
     ob_start();
 
-    $response = $kernel->handle(
-        $request = Illuminate\Http\Request::capture()
-    );
-
+    $response = $kernel->handle($request);
     $response->send();
-
     $kernel->terminate($request, $response);
+
+    unset($request, $response);
 
     return ob_get_clean();
 }
